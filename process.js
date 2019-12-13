a2fd8004d16faa07dc4c6a585516f754ea6de03e
Ryuzetsu <ryuzetsu@DESKTOP-4N4QU34.lan>
2019-12-13 14:03:50 +0200

    govnocode fixed

--- process.js
+++ process.js
@@ -4,14 +4,22 @@ const path_from = process.argv[2];
 const path_to = process.argv[3];
 
 fs.exists(path_to, exists => {
-    if (!exists) fs.mkdir(path_to, err => {if (err) throw err});
+  if (!exists)
+    fs.mkdir(path_to, err => {
+      if (err) throw err
+    });
 });
 
-fs.readdir(path_from, function(err, items) {
-    console.log(items);
-    
-
-    
-    console.log("Total number of processed files: " + items.length);
+fs.readdir(path_from, (err, files) => {
+  files.map(file => {
+    fs.readFile(path_from + file, 'utf8', (err, data) => {
+      fs.writeFile(
+        path_to + file.split('.')[0] + '.res',
+        data.split(/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s/).length,
+        () => { }
+      );
+    });
+  });
+  console.log(`Total number of processed files: ${files.length}`);
 });
 
