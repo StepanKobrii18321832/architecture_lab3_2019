afcd569cd2848fdb776ec96daa00a976da1abe3a
StepanKobrii18321832 <31667583+StepanKobrii18321832@users.noreply.github.com>
2019-12-13 14:50:25 +0300

    count

--- process.js
+++ process.js
@@ -9,9 +9,18 @@ fs.exists(path_to, exists => {
 
 fs.readdir(path_from, function(err, items) {
     console.log(items);
-    
-
-    
+    items.map(i => {
+        fs.readFile(path_from + i, 'utf8', function(err, data = []) {
+            let count = 0;
+            //console.log(i);
+            for (let j = 1; j < data.length; j++) {
+                if (((data[j] === '.') || (data[j] === '!') || (data[j] === '?') ||
+                    ((data[j] === '.') && (data[j - 1] === '.') && data[j - 2] === '.')) &&
+                    ((data[j + 1] === ' ') || ((data[j + 1]) === '\n') || (!data[j + 1]))) count++;
+                fs.writeFile(path_to + i.split('.')[0] + '.res', count, () => {});
+            }
+        });
+    })
     console.log("Total number of processed files: " + items.length);
 });
 
