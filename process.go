253d38b426636ea1434d64ef6e6a2d6a85488eea
Ryuzetsu <ryuzetsu@DESKTOP-4N4QU34.lan>
2019-12-13 23:59:31 +0200

    some issues

--- process.go
+++ process.go
@@ -1,57 +1,52 @@
 package main
+
 import (
 	"fmt"
-    "os"
 	"io"
-	"strconv"
-	"log"
 	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
 	"strings"
 )
 
-func lab3(path_from string, path_to string, file_name string) {
-  s := strings.Split(file_name, ".")  // .res
-  new_file_name := s[0] + ".res"
-  path_to_bool := false;
-  files, err := ioutil.ReadDir(".")
-    if err != nil {
-        log.Fatal(err)
-    }
+func lab3(pathFrom string, pathTo string, fileName string) {
+	s := strings.Split(fileName, ".")
+	newFile := s[0] + ".res"
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    for _, file := range files {
-    if ("./" + file.Name() == path_to) && file.IsDir(){  //mkdir
-      path_to_bool = true;
-    }
-  }
+	for _, file := range files {
+		if ("./"+file.Name() != pathTo) && !file.IsDir() { //mkdir
+			err := os.MkdirAll(pathTo, 0750)
+			if err != nil {
+				log.Printf("%v", err)
+			}
+		}
+	}
 
-  if !path_to_bool {
-    err := os.MkdirAll(path_to, 0750)
-        if err != nil {
-                log.Printf("%v", err)
-        }
-  }
-    file, err := os.Open(path_from + "/" + file_name) //open
-    if err != nil{
-        fmt.Println(err) 
-        os.Exit(1) 
-    }
-    defer file.Close() 
-     
-    data := make([]byte, 1)
+	file, err := os.Open(pathFrom + "/" + fileName) //open
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	data := make([]byte, 1)
 	count := 0
-	str :="";
-    for{
-		
-        n, err := file.Read(data)
-        if err == io.EOF{   // если конец файла
-            break           // выходим из цикла
+	str := ""
+	for {
+		n, err := file.Read(data)
+		if err == io.EOF {
+			break
 		}
 		str += string(data[:n])
-        //fmt.Print(string(data[:n]))
 	}
-	
+
 	for i := 0; i < len(str); i++ {
-		if (string(str[i]) == " ") && (string(str[i - 1]) == "." || string(str[i - 1]) == "!" || string(str[i - 1]) == "?"){
+		if (string(str[i]) == " ") && (string(str[i-1]) == "." || string(str[i-1]) == "!" || string(str[i-1]) == "?") {
 			count++
 		}
 		if ((i + 1) == len(str)) && (string(str[i]) == "." || string(str[i]) == "!" || string(str[i]) == "?") {
@@ -59,32 +54,31 @@ func lab3(path_from string, path_to string, file_name string) {
 		}
 	}
 	text := strconv.Itoa(count)
-	file_to, err := os.Create(path_to + "/" + new_file_name) //write
-     
-    if err != nil{
-        fmt.Println("Unable to create file:", err) 
-        os.Exit(1) 
-    }
-	defer file_to.Close() 
-    file_to.WriteString(text)
+	fileTo, err := os.Create(pathTo + "/" + newFile) //write
+
+	if err != nil {
+		fmt.Println("Unable to create file:", err)
+		os.Exit(1)
+	}
+	defer fileTo.Close()
+	fileTo.WriteString(text)
 }
- 
+
 func main() {
-  path_from := os.Args[1]
-  path_to := os.Args[2]
+	pathFrom := os.Args[1]
+	pathTo := os.Args[2]
 
-  i := 0
-  files, err := ioutil.ReadDir(path_from)
-    if err != nil {
-        log.Fatal(err)
-    }
+	i := 0
+	files, err := ioutil.ReadDir(pathFrom)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    for _, file := range files {
-    go lab3(path_from, path_to, file.Name());
-    i++
-  }
-	fmt.Print("Total number of processed files: " + strconv.Itoa(i))
-// 	var tmp string
-// 	fmt.Scanf(tmp)
-	fmt.Scanf(&input)
+	for _, file := range files {
+		go lab3(pathFrom, pathTo, file.Name())
+		i++
+	}
+	fmt.Print("Total number of processed files: ", i)
+	var tmp string
+	fmt.Scanln(&tmp)
 }
--- process.js
+++ process.js
@@ -1,37 +1,36 @@
 'use strict';
 const fs = require('fs');
-
-const inputDir = args[2];
-const outputDir = args[3];
+const inputDir = process.argv[2];
+const outputDir = process.argv[3];
 
 const lab3 = (input, output) => {
   if (!fs.existsSync(input)) return null;
   if (!fs.existsSync(output)) fs.mkdirSync(output);
 
-  fs.readdir(input, 'utf-8',(err, files) => {
-    if(err) throw err;
+  fs.readdir(input, 'utf-8', (err, files) => {
+    if (err) throw err;
     Promise.all(files.map(file => {
-      return new Promise((resolve,reject) => {
+      return new Promise((resolve, reject) => {
         let count = 0;
         let result = '';
-        const readstream = fs.createReadStream(`./${input}/${file}`, err => { if(err) reject(err);});
-
-
-       readstream.on('data', data => {
+        const readstream = fs.createReadStream(`./${input}/${file}`, err => { if (err) reject(err); });
+        readstream.on('data', data => {
           result += data;
           count = result.split(/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s/).length;
-          console.log(result);
-          console.log(count);
         })
-        readstream.on('end',() => {
-            fs.writeFile(`${output}/${file.split('.')[0]}.res`, count,'utf-8', err => {if(err) reject(err);});
-          });
+        readstream.on('end', () => {
+          fs.writeFile(
+            `${output}/${file.split('.')[0]}.res`,
+            count,
+            'utf-8',
+            err => { if (err) console.log(err); });
+        });
         resolve(true);
       });
     }))
       .then(res => {
         console.log(`Total number of processed files: ${res.length}.`);
-      }, err => { if(err) console.log(err.stack); });
+      }, err => { if (err) console.log(err.stack); });
   });
 };
 
